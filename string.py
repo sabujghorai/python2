26fb637ed1b276b215adc64a81abfb294996369a
sabujghorai <ghoraisabuj4@gmail.com>
2025-11-30 01:41:49 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,17 @@
+# creating a string
+p = "hello how are you all"
+print(p)
+# it's called concatination
+a = "hey"
+b = "I"
+c = "am"
+d = "good"
+k = a+" "+b+" "+c+" "+d # we can add blank space also but it should be in single,double or triple cotes
+print(k)
+print(len(k)) # we can calculate the length of the string by using len function
+
+# indexing
+
+a = "sabuj ghorai"
+ch = a[4]
+print(ch) 
